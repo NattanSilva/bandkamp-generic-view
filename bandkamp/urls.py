3d5638313a350f4ecf70b6e8e66b675e2fe5871d
NattanSilva <natanxboy@gmail.com>
2023-03-02 01:43:07 -0300

    Fix: docs route and DER diagram

--- bandkamp/urls.py
+++ bandkamp/urls.py
@@ -26,9 +26,5 @@ urlpatterns = [
     path("api/", include("users.urls")),
     path("api/", include("albums.urls")),
     # path("api/", include("songs.urls")),
-    # Acessa o download do schema
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    # Opcionais (escolha 1)
-    path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc')
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
